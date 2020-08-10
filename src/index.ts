447dfee1e70a3e18dc305853817402dd40ab01ef
mateus4k <mateus4k@protonmail.ch>
2020-08-10 20:38:08 -0300

    chore(server): improve configs

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,9 @@ import { cep as cepResolver } from './resolvers';
 const server = new ApolloServer({
   typeDefs: cepType,
   resolvers: cepResolver,
-  introspection: true,
-  playground: true,
 });
 
 server.listen(process.env.PORT || 4000).then(({ url }) => {
   // eslint-disable-next-line no-console
-  console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀 Server ready at ${url}`);
 });
